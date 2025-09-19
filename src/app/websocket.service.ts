5cb42875cee334d85619fd36c1f21bb03b7d2b6f
l0nkDev <43931415+l0nkDev@users.noreply.github.com>
2025-09-19 16:33:59 -0400

    fixes

--- src/app/websocket.service.ts
+++ src/app/websocket.service.ts
@@ -1,32 +1,40 @@
 import { Subject, Observable } from 'rxjs';
 
 export class WebSocketService {
-  private socket: WebSocket;
+  private socket: WebSocket | null = null;
   private messageSubject = new Subject<string>();
 
   constructor(sessionId: string) {
-    this.socket = new WebSocket(`wss://websocket.lonk.dev/ws/session/${sessionId}`); // Adjust URL as needed
+    this.init(sessionId);
+  }
 
-    this.socket.onopen = (event) => {
-      console.log('WebSocket connection opened:', event);
-    };
+  init(sessionId: string) {
+    if (sessionId !== '') {
+      this.socket = new WebSocket(`wss://websocket.lonk.dev/ws/session/${sessionId}`); // Adjust URL as needed
 
-    this.socket.onmessage = (event) => {
-      console.log('Message from server:', JSON.parse(event.data));
-      this.messageSubject.next(event.data);
-    };
+      this.socket.onopen = (event) => {
+        console.log('WebSocket connection opened:', event);
+      };
 
-    this.socket.onclose = (event) => {
-      console.log('WebSocket connection closed:', event);
-    };
+      this.socket.onmessage = (event) => {
+        console.log('Message from server:', JSON.parse(event.data));
+        this.messageSubject.next(event.data);
+      };
 
-    this.socket.onerror = (error) => {
-      console.error('WebSocket error:', error);
-    };
+      this.socket.onclose = (event) => {
+        console.log('WebSocket connection closed:', event);
+        this.socket = new WebSocket(`wss://websocket.lonk.dev/ws/session/${sessionId}`);
+      };
+
+      this.socket.onerror = (error) => {
+        console.error('WebSocket error:', error);
+        this.socket = new WebSocket(`wss://websocket.lonk.dev/ws/session/${sessionId}`);
+      };
+    }
   }
 
   sendMessage(message: string): void {
-    this.socket.send(JSON.stringify({ message }));
+    this.socket?.send(JSON.stringify({ message }));
   }
 
   getMessages(): Observable<string> {
